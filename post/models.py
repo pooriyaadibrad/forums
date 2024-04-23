5bdaeecd7af17d93af0a651a5b4988a65b27ac17
pooriya <adibradp@gamil.com>
2024-04-23 21:14:46 +0330

    final work in this node template complete

--- post/models.py
+++ post/models.py
@@ -9,11 +9,11 @@ class post(models.Model):
     title = models.CharField(max_length=100,default='',null=False,blank=False)
     #author = models.CharField(max_length=100,default='',null=False,blank=False)
     content = models.TextField(default='',null=True,blank=True)
-    image = models.ImageField(upload_to='media/',default='default.jpg')
+    image = models.ImageField(upload_to='media/post/',default='default.jpg')
     pub_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('category',on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.title} , {self.pub_date} '
+        return f'{self.title} , {self.pub_date}'
 class category(models.Model):
     name = models.CharField(max_length=100,default='',null=False,blank=False)
     def __str__(self):
--- post/views.py
+++ post/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = models.post.objects.all()
+    return render(request, 'index.html',context={'posts':posts})
 def blog(request):
     return render(request, 'blog.html',context={})
 def contact(request):
