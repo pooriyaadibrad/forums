bc2acdd8542becd9e25c2e0a8e17fd262b2b3d92
pooriya <adibradp@gamil.com>
2024-04-23 19:13:04 +0330

    master page fixed and blog and index too and use it !

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR , '/media')
+
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'post/static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- post/models.py
+++ post/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+this place we create models for posts and cattegory for every post
+"""
+class post(models.Model):
+    title = models.CharField(max_length=100,default='',null=False,blank=False)
+    #author = models.CharField(max_length=100,default='',null=False,blank=False)
+    content = models.TextField(default='',null=True,blank=True)
+    image = models.ImageField(upload_to='media/',default='default.jpg')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('category',on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.title} , {self.pub_date} '
+class category(models.Model):
+    name = models.CharField(max_length=100,default='',null=False,blank=False)
+    def __str__(self):
+        return f'{self.name}'
+
+"""
+this place you can create custom user models for every post for every category and create assiciated model for every post
+class users(models.Model):
+    name = models.CharField(max_length=100,default='',null=False,blank=False)
+    password = models.CharField(max_length=100,default='',null=False,blank=False)
+    email = models.CharField(max_length=100,default='',null=False,blank=False)
+    def __str__(self):
+        return f'{self.name}'
+"""
+
+
